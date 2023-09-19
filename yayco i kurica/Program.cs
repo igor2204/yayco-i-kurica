e4d2d00afb02b1b5a7ba45d01da1b2604f93620d
Цомкалов Игорь <igorcomkalov@gmail.com>
2023-09-19 12:06:33 +0300

    Добавьте файлы проекта.

---
+++ yayco i kurica/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Thread chicken = new(MyThreadChicken);
+        chicken.Start();
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Курица");
+        MyThreadAnswer(chicken);
+    }
+    static void MyThreadChicken()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Яйцо");
+    }
+    static void MyThreadAnswer(Thread chicken)
+    {
+
+        bool ans = chicken.IsAlive;
+        Console.WriteLine(ans ? "Победило Яйцо." : "Победила Курица.");
+    }
+}
